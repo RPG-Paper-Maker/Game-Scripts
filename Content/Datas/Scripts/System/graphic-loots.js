9639ecfacf96b4d93c8024cdb9ededda0b1b8220
Marie Laporte <mmarie.laporte@gmail.com>
2019-07-18 12:43:33 +0200

    [FIX] License bad removed

--- Content/Datas/Scripts/System/graphic-loots.js
+++ Content/Datas/Scripts/System/graphic-loots.js
@@ -8,17 +8,6 @@
     See RPG Paper Maker EULA here:
         http://rpg-paper-maker.com/index.php/eula.
 */
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    RPG Paper Maker is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 // -------------------------------------------------------
 //
