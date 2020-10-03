ecb8d6f1f77976aa0def6ccdcd2fe38a16f5e1cc
Marie Laporte <mmarie.laporte@gmail.com>
2020-10-03 02:59:50 +0200

    [CLEANING] Texture several + System WindowSkin

--- Content/Datas/Scripts/System/system-window-skin.js
+++ Content/Datas/Scripts/System/system-window-skin.js
@@ -9,25 +9,44 @@
         http://rpg-paper-maker.com/index.php/eula.
 */
 
-// -------------------------------------------------------
-//
-//  CLASS SystemWindowSkin
-//
-// -------------------------------------------------------
-
 /** @class
-*   A windowskin of the game.
+*   A window skin of the game
+*   @property {number} pictureID The picture ID used for the window skin
+*   @property {number[]} borderTopLeft The top left border rect
+*   @property {number[]} borderTopRight The top right border rect
+*   @property {number[]} borderBotLeft The bot left border rect
+*   @property {number[]} borderBotRight The bot right border rect
+*   @property {number[]} borderLeft The left border rect
+*   @property {number[]} borderRight The right border rect
+*   @property {number[]} borderTop The top border rect
+*   @property {number[]} borderBot The bot border rect
+*   @property {number[]} background The background rect
+*   @property {number[]} backgroundSelection The background when window is 
+*   selected rect
+*   @property {boolean} backgroundRepeat If checked, the window skin background 
+*   is repeated, if not, it's stretched
+*   @property {number[]} arrowEndMessage The arrow for end of messages rect
+*   @property {number[]} arrowTargetSelection The arrow for target selection 
+*   rect
+*   @property {number[]} arrowUpDown The arrow for up and down rect
+*   @property {number[]} textNormal The normal text rect
+*   @property {number[]} textCritical The critical text rect
+*   @property {number[]} textHeal The heal text rect
+*   @property {number[]} textMiss The miss text rect
 */
-function SystemWindowSkin() {
+class SystemWindowSkin
+{
+    constructor()
+    {
 
-}
-
-SystemWindowSkin.prototype = {
-
-    /** Read the JSON associated to the weapon.
-    *   @param {Object} json Json object describing the object.
+    }
+    
+    // -------------------------------------------------------
+    /** Read the JSON associated to the window skin
+    *   @param {Object} json Json object describing the window skin
     */
-    readJSON: function(json) {
+    readJSON(json)
+    {
         this.pictureID = json.pid;
         this.borderTopLeft = json.tl;
         this.borderTopRight = json.tr;
@@ -47,172 +66,222 @@ SystemWindowSkin.prototype = {
         this.textCritical = json.tc;
         this.textHeal = json.th;
         this.textMiss = json.tm;
-    },
+    }
 
     // -------------------------------------------------------
-
-    updatePicture: function() {
-        this.picture = Picture2D.createImage(RPM.datasGame.pictures.get(PictureKind
-            .WindowSkins, this.pictureID), PictureKind.WindowSkins);
+    /** Update the window skin picture ID
+    */
+    updatePicture()
+    {
+        this.picture = Picture2D.createImage(RPM.datasGame.pictures.get(
+            PictureKind.WindowSkins, this.pictureID), PictureKind.WindowSkins);
         this.picture.stretch = true;
-    },
+    }
 
     // -------------------------------------------------------
-
-    drawElement: function(r, x, y, w, h, zoom) {
-        if (!w) {
-            w = r[2];
-        }
-        if (!h) {
-            h = r[3];
-        }
-        if (RPM.isUndefined(zoom))
-        {
-            zoom = 1.0;
-        }
-
+    /** Draw any element of the window skin box with the cut picture
+    *   @param {number[]} r The rect source
+    *   @param {number} x The x target
+    *   @param {number} y The y target
+    *   @param {number} [w=r[2]] The w target
+    *   @param {number} [h=r[3]] The h target
+    *   @param {number} [zoom=1.0] The zoom to apply of target size
+    */
+    drawElement(r, x, y, w = r[2], h = r[3], zoom = 1.0)
+    {
         this.picture.draw(x, y, w * zoom, h * zoom, r[0], r[1], r[2], r[3]);
-    },
+    }
 
     // -------------------------------------------------------
-
-    drawBoxBackground: function(background, rect) {
-        if (this.backgroundRepeat) {
-            var x, y, w, h, l, m;
-            for (x = rect[0] + this.borderTopLeft[2], l = rect[0] + rect[2] -
-                 this.borderTopRight[2] - 1; x < l; x += background[2])
+    /** Draw the background box
+    *   @param {number[]} background The background source rect
+    *   @param {number[]} rect The final box rect
+    */
+    drawBoxBackground(background, rect)
+    {
+        if (this.backgroundRepeat)
+        {
+            let y, m, w, h;
+            for (let x = rect[0] + this.borderTopLeft[2], l = rect[0] + rect[2] 
+                - this.borderTopRight[2] - 1; x < l; x += background[2])
             {
                 for (y = rect[1] + this.borderTopLeft[3], m = rect[1] + rect[3]
-                     - this.borderBotLeft[3] - 1; y < m; y += background[3])
+                    - this.borderBotLeft[3] - 1; y < m; y += background[3])
                 {
                     w = x + background[2] < l ? background[2] : l - x + 1;
                     h = y + background[3] < m ? background[3] : m - y + 1;
                     this.drawElement(background, x, y, w, h);
                 }
             }
-        } else {
-            this.drawElement(background, rect[0] + this
-                .borderTopLeft[2], rect[1] + this.borderTopLeft[3], rect[2] -
-                this.borderTopLeft[2] - this.borderBotRight[2], rect[3] - this
-                .borderTopLeft[3] - this.borderBotRight[3]);
+        } else
+        {
+            this.drawElement(background, rect[0] + this.borderTopLeft[2], rect[1
+                ] + this.borderTopLeft[3], rect[2] - this.borderTopLeft[2] - 
+                this.borderBotRight[2], rect[3] - this.borderTopLeft[3] - this
+                .borderBotRight[3]);
         }
-    },
+    }
 
     // -------------------------------------------------------
-
-    drawBox: function(rect, selected, bordersVisible) {
-        var x, y, w, h, l, m;
-
-        if (bordersVisible) {
+    /** Draw the box
+    *   @param {number[]} rect The final box rect
+    *   @param {boolean} selected Indicate if the box is selected
+    *   @param {boolean} bordersVisible Indicate if the borders of the box are visible
+    */
+    drawBox(rect, selected, bordersVisible)
+    {
+        if (bordersVisible) 
+        {
             // Corners
             this.drawElement(this.borderTopLeft, rect[0], rect[1]);
-            this.drawElement(this.borderTopRight, rect[0] + rect[2] -
-                this.borderTopRight[2], rect[1]);
-            this.drawElement(this.borderBotLeft, rect[0], rect[1] +
-                rect[3] - this.borderBotLeft[3]);
-            this.drawElement(this.borderBotRight, rect[0] + rect[2] -
-                this.borderBotRight[2], rect[1] + rect[3] - this.borderBotRight[
-                3]);
+            this.drawElement(this.borderTopRight, rect[0] + rect[2] - this
+                .borderTopRight[2], rect[1]);
+            this.drawElement(this.borderBotLeft, rect[0], rect[1] + rect[3] - 
+                this.borderBotLeft[3]);
+            this.drawElement(this.borderBotRight, rect[0] + rect[2] - this
+                .borderBotRight[2], rect[1] + rect[3] - this.borderBotRight[3]);
 
             // Borders
-            x = rect[0];
+            let x = rect[0];
+            let y, l;
             for (y = rect[1] + this.borderTopLeft[3], l = rect[1] + rect[3] -
-                 this.borderBotLeft[3] - 1; y < l; y += this.borderLeft[3])
+                this.borderBotLeft[3] - 1; y < l; y += this.borderLeft[3])
             {
-                if (y + this.borderLeft[3] < l) {
+                if (y + this.borderLeft[3] < l)
+                {
                     this.drawElement(this.borderLeft, x, y);
-                } else {
+                } else
+                {
                     this.drawElement(this.borderLeft, x, y, this
                         .borderLeft[2], l - y + 1);
                 }
             }
             x = rect[0] + rect[2] - this.borderTopRight[2];
             for (y = rect[1] + this.borderTopLeft[3], l = rect[1] + rect[3] -
-                 this.borderBotLeft[3] - 1; y < l; y += this.borderRight[3])
+                this.borderBotLeft[3] - 1; y < l; y += this.borderRight[3])
             {
-                if (y + this.borderRight[3] < l) {
+                if (y + this.borderRight[3] < l)
+                {
                     this.drawElement(this.borderRight, x, y);
-                } else {
-                    this.drawElement(this.borderRight, x, y, this
-                        .borderRight[2], l - y + 1);
+                } else
+                {
+                    this.drawElement(this.borderRight, x, y, this.borderRight[2]
+                        , l - y + 1);
                 }
             }
             y = rect[1];
             for (x = rect[0] + this.borderTopLeft[2], l = rect[0] + rect[2] -
-                 this.borderTopRight[2] - 1; x < l; x += this.borderTop[2])
+                this.borderTopRight[2] - 1; x < l; x += this.borderTop[2])
             {
-                if (x + this.borderTop[2] < l) {
+                if (x + this.borderTop[2] < l)
+                {
                     this.drawElement(this.borderTop, x, y);
-                } else {
-                    this.drawElement(this.borderTop, x, y, l - x + 1,
-                        this.borderTop[3]);
+                } else
+                {
+                    this.drawElement(this.borderTop, x, y, l - x + 1, this
+                        .borderTop[3]);
                 }
             }
             y = rect[1] + rect[3] - this.borderBotLeft[3];
             for (x = rect[0] + this.borderBotLeft[2], l = rect[0] + rect[2] -
-                 this.borderBotRight[2] - 1; x < l; x += this.borderBot[2])
+                this.borderBotRight[2] - 1; x < l; x += this.borderBot[2])
             {
-                if (x + this.borderBot[2] < l) {
+                if (x + this.borderBot[2] < l)
+                {
                     this.drawElement(this.borderBot, x, y);
-                } else {
-                    this.drawElement(this.borderBot, x, y, l - x + 1,
-                        this.borderBot[3]);
+                } else
+                {
+                    this.drawElement(this.borderBot, x, y, l - x + 1, this
+                        .borderBot[3]);
                 }
             }
         }
 
         // Background
         this.drawBoxBackground(this.background, rect);
-        if (selected) {
+        if (selected)
+        {
             this.drawBoxBackground(this.backgroundSelection, rect);
         }
-    },
+    }
 
     // -------------------------------------------------------
-
-    drawArrowTarget: function(frame, x, y, positionResize) {
-        var width = this.arrowTargetSelection[2] / RPM.FRAMES;
+    /** Draw the arrow for targets
+    *   @param {number} frame The current frame to draw
+    *   @param {number} x The x position
+    *   @param {number} y The y position
+    *   @param {boolean} positionResize Indicate if the position picture needs 
+    *   to be resize (resolution)
+    */
+    drawArrowTarget(frame, x, y, positionResize)
+    {
+        let width = this.arrowTargetSelection[2] / RPM.FRAMES;
         this.picture.draw(x - (width / 2), y, width, this.arrowTargetSelection
             [3],this.arrowTargetSelection[0] + (frame * width), this
             .arrowTargetSelection[1], width, this.arrowTargetSelection[3],
             positionResize);
-    },
+    }
 
     // -------------------------------------------------------
-
-    drawArrowMessage: function(frame, x, y) {
-        var width = this.arrowEndMessage[2] / RPM.FRAMES;
+    /** Draw the arrow for end of messages
+    *   @param {number} frame The current frame to draw
+    *   @param {number} x The x position
+    *   @param {number} y The y position
+    */
+    drawArrowMessage(frame, x, y)
+    {
+        let width = this.arrowEndMessage[2] / RPM.FRAMES;
         this.picture.draw(x - (width / 2), y, width, this.arrowEndMessage[3],
             this.arrowEndMessage[0] + (frame * width), this.arrowEndMessage[1],
             width, this.arrowEndMessage[3]);
-    },
+    }
 
     // -------------------------------------------------------
-
-    drawDamagesNumber: function(damage, x, y, rect, zoom) {
-        var digits = ("" + damage).split("").map(Number);
-        var width = rect[2] / 10;
-        var height = rect[3];
+    /** Draw a damage number
+    *   @param {number} damage The damage number to display
+    *   @param {number} x The x position
+    *   @param {number} y The y position
+    *   @param {number[]} rect The source rect
+    *   @param {number} zoom The zoom to apply on damages
+    */
+    drawDamagesNumber(damage, x, y, rect, zoom)
+    {
+        let digits = ("" + damage).split("").map(Number);
+        let width = rect[2] / 10;
+        let height = rect[3];
         this.picture.stretch = false;
-        for (var i = 0, l = digits.length; i < l; i++) {
+        for (let i = 0, l = digits.length; i < l; i++)
+        {
             this.picture.draw(x + ((i - ((l -1) / 2)) * (RPM.getScreenMinXY(
                 width) * zoom)), y, width * zoom, height * zoom, rect[0] + (
                 digits[i] * width), rect[1], width, height, false);
         }
         this.picture.stretch = true;
-    },
+    }
 
     // -------------------------------------------------------
-
-    drawDamages: function(damage, x, y, isCrit, isMiss, zoom) {
-        if (isMiss) {
+    /** Draw a damage number according to the kind of damages
+    *   @param {number} damage The damage number to display
+    *   @param {number} x The x position
+    *   @param {number} y The y position
+    *   @param {boolean} isCrit Indicate if the damages are a critical hit
+    *   @param {boolean} isMiss Indicate if the damages are a missed hit
+    *   @param {number} zoom The zoom to apply on damages
+    */
+    drawDamages(damage, x, y, isCrit, isMiss, zoom)
+    {
+        if (isMiss)
+        {
             this.drawElement(this.textMiss, x - this.textMiss[2] / 2, y, null,
                 null, zoom);
-        } else if (damage < 0) {
+        } else if (damage < 0)
+        {
             this.drawDamagesNumber(damage, x, y, this.textHeal, zoom);
-        } else if (isCrit) {
+        } else if (isCrit)
+        {
             this.drawDamagesNumber(damage, x, y, this.textCritical, zoom);
-        } else {
+        } else
+        {
             this.drawDamagesNumber(damage, x, y, this.textNormal, zoom);
         }
     }
--- Content/Datas/Scripts/System/texture-several.js
+++ Content/Datas/Scripts/System/texture-several.js
@@ -9,131 +9,138 @@
         http://rpg-paper-maker.com/index.php/eula.
 */
 
-// -------------------------------------------------------
-//
-//  CLASS TextureSeveral
-//
-// -------------------------------------------------------
-
 /** @class
-*   Several textures in a single file.
-*   @property {THREE.Material} texture The generated texture.
-*   @property {number} beginID The begining texture ID.
-*   @property {number[]} beginPoint The begining texture point offset.
-*   @property {number} endID The ending texture ID.
-*   @property {number[]} endPoint The ending texture point offset.
-*   @property {number[][]} list List of each offset point.
+*   Several textures in a single file
+*   @property {number[][]} list List of each offset point, default is an empty 
+*   @property {THREE.Material} texture The generated texture, default is null
+*   @property {number} beginID The begining texture ID
+*   @property {number[]} beginPoint The begining texture point offset
+*   @property {number} endID The ending texture ID
+*   @property {number[]} endPoint The ending texture point offset
+*   array
 */
 
-function TextureSeveral(){
-    this.list = new Array;
-    this.texture = null;
-}
+class TextureSeveral
 
-TextureSeveral.prototype = {
+{   constructor()
+    {
+        this.list = new Array;
+        this.texture = null;
+    }
 
-    /** Set the begining texture.
-    *   @param {number} id The begining texture ID.
-    *   @param {number[]} point The begining texture point offset.
+    // -------------------------------------------------------
+    /** Set the begining texture
+    *   @param {number} id The begining texture ID
+    *   @param {number[]} point The begining texture point offset
     */
-    setBegin: function(id, point){
+    setBegin(id, point)
+    {
         this.beginID = id;
         this.beginPoint = point;
-    },
+    }
 
     // -------------------------------------------------------
-
-    /** Set the ending texture.
-    *   @param {number} id The ending texture ID.
-    *   @param {number[]} point The ending texture point offset.
+    /** Set the ending texture
+    *   @param {number} id The ending texture ID
+    *   @param {number[]} point The ending texture point offset
     */
-    setEnd: function(id, point){
+    setEnd(id, point)
+    {
         this.endID = id;
         this.endPoint = point;
-    },
+    }
 
     // -------------------------------------------------------
-
-    /** Check which point is on top.
+    /** Check which point is on top
     *   @param {number[]} rect
     *   @param {number[]} point
     */
-    isSup: function(rect, point) {
-        if (!rect) {
+    isSup(rect, point)
+    {
+        if (!rect)
+        {
             return true;
         }
-
         if (rect[1] > point[1])
+        {
             return true;
-        else if (rect[1] === point[1])
+        } else if (rect[1] === point[1])
+        {
             return rect[0] >= point[0];
+        }
 
         return false;
-    },
+    }
 
     // -------------------------------------------------------
 
-    /** Check which point is on bot.
+    /** Check which point is on bot
     *   @param {number[]} rect
     *   @param {number[]} point
     */
-    isInf: function(rect, point) {
-        if (!rect) {
+    isInf(rect, point)
+    {
+        if (!rect)
+        {
             return true;
         }
 
         if (rect[1] < point[1])
+        {
             return true;
-        else if (rect[1] === point[1])
+        } else if (rect[1] === point[1])
+        {
             return rect[0] <= point[0];
+        }
 
         return false;
-    },
+    }
 
     // -------------------------------------------------------
-
-    /** Check if a couple (id, rect) is inside this texture.
+    /** Check if a couple (id, rect) is inside this texture
     *   @param {number} id
     *   @param {number[]} rect
     */
-    isInTexture: function(id, rect){
-        if (id >= this.beginID && id <= this.endID) {
-            if (id === this.beginID) {
-                if (id === this.endID) {
-                    return this.isSup(rect, this.beginPoint) &&
-                           this.isInf(rect, this.endPoint);
-                }
-                else
-                    return this.isSup(rect, this.beginPoint);
-            }
-            else if (id < this.endID)
+    isInTexture(id, rect)
+    {
+        if (id >= this.beginID && id <= this.endID)
+        {
+            if (id === this.beginID)
+            {
+                return id === this.endID ? (this.isSup(rect, this.beginPoint) && 
+                    this.isInf(rect, this.endPoint)) : this.isSup(rect, this
+                    .beginPoint);
+            } else if (id < this.endID)
+            {
                 return true;
-            else
+            } else
+            {
                 return this.isInf(rect, this.endPoint);
+            }
         }
         return false;
-    },
+    }
 
     // -------------------------------------------------------
-
-    /** Add a couple (id, point) inside the list.
+    /** Add a couple (id, point) inside the list
     *   @param {number} id
     *   @param {number[]} point
     */
-    addToList: function(id, point){
+    addToList(id, point)
+    {
         this.list.push([id, point]);
-    },
+    }
 
     // -------------------------------------------------------
-
-    /** Get the offset of couple (id, rect).
+    /** Get the offset of couple (id, rect)
     *   @param {number} id
     *   @param {number[]} rect
     */
-    getOffset: function(id, rect) {
-        var i, l, pair, point;
-
-        for (i = 0, l = this.list.length; i < l; i++) {
+    getOffset(id, rect)
+    {
+        let pair, point;
+        for (let i = 0, l = this.list.length; i < l; i++)
+        {
             pair = this.list[i];
             point = pair[1];
             if (id === pair[0] && (rect === null || (point[0] === rect[0] &&
@@ -142,7 +149,6 @@ TextureSeveral.prototype = {
                 return i;
             }
         }
-
         return -1;
     }
 }
