fe7ecd07e1c18b90935cc6132d26dc088b7d8b49
Kino <kinoarose@gmail.com>
2020-12-14 03:09:30 +0000

    Start refactor of battle class

---
+++ Content/Datas/Scripts/src/Scene/BattleAnimation.ts
@@ -0,0 +1,26 @@
+
+/*
+    RPG Paper Maker Copyright (C) 2017-2020 Wano
+
+    RPG Paper Maker engine is under proprietary license.
+    This source code is also copyrighted.
+
+    Use Commercial edition for commercial use of your games.
+    See RPG Paper Maker EULA here:
+        http://rpg-paper-maker.com/index.php/eula.
+*/
+
+// -------------------------------------------------------
+//
+//  CLASS SceneBattle
+//
+//  Step 2 :
+//      SubStep 0 : Animation user + animation sprite
+//      SubStep 1 : Animation target
+//      SubStep 2 : Damages
+//
+// -------------------------------------------------------
+
+class BattleAnimation {
+
+}
