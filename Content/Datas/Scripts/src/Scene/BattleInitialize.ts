022a81f875a79bfbd0e5ee90af1666657d0fd1fb
Kino <kinoarose@gmail.com>
2020-12-14 03:09:30 +0000

    Start refactor of battle class

---
+++ Content/Datas/Scripts/src/Scene/BattleInitialize.ts
@@ -0,0 +1,23 @@
+
+/*
+    RPG Paper Maker Copyright (C) 2017-2020 Wano
+
+    RPG Paper Maker engine is under proprietary license.
+    This source code is also copyrighted.
+
+    Use Commercial edition for commercial use of your games.
+    See RPG Paper Maker EULA here:
+        http://rpg-paper-maker.com/index.php/eula.
+*/
+
+// -------------------------------------------------------
+//
+//  CLASS SceneBattle
+//
+//  Step 0 : Initialization of the battle, camera movment (transition),
+//  allies/ennemies comming.
+//
+// -------------------------------------------------------
+class BattleInitialize {
+
+}
---
+++ Content/Datas/Scripts/src/Scene/BattleVictory.ts
@@ -0,0 +1,27 @@
+/*
+    RPG Paper Maker Copyright (C) 2017-2020 Wano
+
+    RPG Paper Maker engine is under proprietary license.
+    This source code is also copyrighted.
+
+    Use Commercial edition for commercial use of your games.
+    See RPG Paper Maker EULA here:
+        http://rpg-paper-maker.com/index.php/eula.
+*/
+
+// -------------------------------------------------------
+//
+//  CLASS BattleVictory
+//
+//  Step 4 : End of battle
+//      SubStep 0 : Victory message
+//      SubStep 1 : Experience update
+//      SubStep 2 : Level up
+//      SubStep 3 : End transition
+//      SubStep 4 : Defeat message
+//
+// -------------------------------------------------------
+
+class BattleVictory {
+
+}
