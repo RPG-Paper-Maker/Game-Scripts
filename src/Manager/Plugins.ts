f1220e79859c61bfec38b14d34f00208e3869c3a
Trico-Everfire <Trico-Everfire@users.noreply.github.com>
2021-01-15 17:50:21 +0100

    Added prototypeOverwrite support to Manager/plugin.ts
    The prototypeManager (may wanna change the name) replaces the need to prototype manually, this has a cleaner workflow and results in easier to understand plugins.

--- src/Manager/Plugins.ts
+++ src/Manager/Plugins.ts
@@ -182,6 +182,7 @@ class Plugins {
         this.plugins[parent].parameters = { ...par, ...chi };
         */
     }
+<<<<<<< HEAD
 
     /**
      *  @static
@@ -198,6 +199,20 @@ class Plugins {
         classObject: T, prototypeName: LT | TR | M, prototype:T["prototype"][LT] 
         | T[LT] | LM, staticType :boolean = false, overwrite:boolean = false, 
         loadBefore: boolean = true) {
+=======
+    /**
+     * @static
+     * @usage This function is used to inject/overwrite original class methods and variables.
+     * @experimental This function is experimental and has yet to be thoroughly tested.
+     * @param classObject The class or newable function that you want to inject/overwrite a variable/method into.
+     * @param prototypeName The variable/method name you want to overwrite/inject code into.
+     * @param prototype The new variable/method you want to inject/overwrite.
+     * @param staticType Sets rather this is a static method/variable or a non static method/variable (NOTE: Both a static and non static variable/method can exist at the same time with the same name.) (DEFAULT: false)
+     * @param overwrite (METHODS ONLY) Should call original method's code or overwrite original method. (DEFAULT: false)
+     * @param loadBefore (METHODS ONLY) Should original method's code be executed before or after your code (NOTE: This is obviously disabled if param overwrite is set to true.) (DEFAULT: true)
+     */
+    static prototypeOverwrite<T extends NewableFunction, M extends keyof T,LT extends keyof T["prototype"],TR = string,LM = NewableFunction>(classObject:T, prototypeName:LT | TR | M, prototype:T["prototype"][LT] | T[LT] | LM,staticType:boolean = false,overwrite:boolean = false,loadBefore:boolean = true){
+>>>>>>> f257966 (Added prototypeOverwrite support to Manager/plugin.ts)
         let TheAnyPrototype:any = prototype; //force any type, system will not accept otherwise!
         if(!staticType){
             let classPrototype = classObject.prototype[prototypeName];
@@ -225,11 +240,14 @@ class Plugins {
             let classAnyObject: any = classObject;  //force any type, system will not accept otherwise!
             let classMethod = classAnyObject[prototypeName];
             if(classMethod instanceof Function){
+<<<<<<< HEAD
                 if(overwrite){
                     classAnyObject[prototypeName] = function(...args){
                         TheAnyPrototype.call(this,...args);
                     }
                 } else 
+=======
+>>>>>>> f257966 (Added prototypeOverwrite support to Manager/plugin.ts)
                 if(loadBefore){
                     classAnyObject[prototypeName] = function(...args){
                         classMethod.call(this,...args);
