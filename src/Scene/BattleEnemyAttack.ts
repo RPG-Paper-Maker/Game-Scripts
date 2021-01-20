d7c410840ad3f259df66f67ed88a522d1f4e594a
Marie Laporte <mmarie.laporte@gmail.com>
2021-01-20 13:13:19 +0100

    Update copyright to 2021

--- src/Scene/BattleEnemyAttack.ts
+++ src/Scene/BattleEnemyAttack.ts
@@ -1,6 +1,6 @@
 
 /*
-    RPG Paper Maker Copyright (C) 2017-2020 Wano
+    RPG Paper Maker Copyright (C) 2017-2021 Wano
 
     RPG Paper Maker engine is under proprietary license.
     This source code is also copyrighted.
