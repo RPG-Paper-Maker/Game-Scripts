d9db018d3b6a12d3aaf1fe19a700e287f39e5e4b
Trico-Everfire <Trico-Everfire@users.noreply.github.com>
2021-01-28 18:55:56 +0100

    fixed super call no longer needing "this"in parameters.

--- src/Manager/Plugins.ts
+++ src/Manager/Plugins.ts
@@ -204,19 +204,19 @@ class Plugins {
             if(classPrototype instanceof Function){
                 if(overwrite){
                     classObject.prototype[prototypeName] = function(...args){
-                        this.super = classPrototype.call;
+                        this.super = (...arggs)=>{classPrototype.call(this,...arggs)};
                         return TheAnyPrototype.call(this,...args);
                     }
                 } else if(loadBefore){
                     classObject.prototype[prototypeName] = function(...args){
                         let result = classPrototype.call(this,...args);
-                        this.super = classPrototype.call;
+                        this.super = (...arggs)=>{classPrototype.call(this,...arggs)};
                         this.callResult = result;
                        return TheAnyPrototype.call(this,...args);
                     }
                 } else {
                     classObject.prototype[prototypeName] = function(...args){
-                        this.super = classPrototype.call;
+                        this.super = (...arggs)=>{classPrototype.call(this,...arggs)};
                         TheAnyPrototype.call(this,...args);
                        return classPrototype.call(this,...args);
                     }
@@ -230,20 +230,20 @@ class Plugins {
             if(classMethod instanceof Function){
                 if(overwrite){
                     classAnyObject[prototypeName] = function(...args){
-                        this.super = classMethod.call;
+                        this.super = (...arggs)=>{classMethod.call(this,...arggs)};
                        return TheAnyPrototype.call(this,...args);
                     }
                 } else 
                 if(loadBefore){
                     classAnyObject[prototypeName] = function(...args){
                        let result = classMethod.call(this,...args);
-                       this.super = classMethod.call;
+                       this.super = (...arggs)=>{classMethod.call(this,...arggs)};
                        this.callResult = result;
                       return TheAnyPrototype.call(this,...args);
                     }
                 } else {
                     classAnyObject[prototypeName] = function(...args){
-                        this.super = classMethod.call;
+                        this.super = (...arggs)=>{classMethod.call(this,...arggs)};
                         TheAnyPrototype.call(this,...args);
                        return classMethod.call(this,...args);
                     }
