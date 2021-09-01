cdf802013ca62728ef6baa7b9cff77fb6eeb80da
Marie Laporte <mmarie.laporte@gmail.com>
2021-09-01 14:22:49 +0200

    Update bash files + package.json

--- compile.sh
+++ compile.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
-rm -R ./Content/Datas/Scripts/System
+#!/bin/bash
+path="./Content/Datas/Scripts/System"
+if [[ -d "${path}" ]]
+then
+	rm -R "${path}"
+fi
 npx tsc
 cp ./src/Definitions.d.ts ./Content/Datas/Scripts/System/Definitions.d.ts
 ./module-fix.sh
