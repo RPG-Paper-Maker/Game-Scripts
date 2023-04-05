46c82fd3348cf8ac2957dfac35fefe06b6bed778
Marie Laporte <mmarie.laporte@gmail.com>
2023-04-05 14:14:17 +0200

    [FIX] Semi transparent textures not correctly displayed

--- src/Core/MapObject.ts
+++ src/Core/MapObject.ts
@@ -617,6 +617,7 @@ class MapObject {
             }
             this.mesh.position.set(this.position.x, this.position.y, this
                 .position.z);
+            this.mesh.renderOrder = 9999;
             this.boundingBoxSettings = objCollision[1][0];
             if (this.boundingBoxSettings) {
                 if (this.currentStateInstance.graphicID === 0) {
