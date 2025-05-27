f0421b29719ef1380c068f0b03127ced3a322873
Marie Laporte <mmarie.laporte@gmail.com>
2025-05-27 10:24:09 +0200

    Monster troop: use "mid" instead of "id" for monster id

--- src/System/TroopMonster.ts
+++ src/System/TroopMonster.ts
@@ -9,41 +9,42 @@
         http://rpg-paper-maker.com/index.php/eula.
 */
 
-import { System } from "..";
-import { Utils } from "../Common";
-import { Base } from "./Base";
+import { System } from '..';
+import { Utils } from '../Common';
+import { Base } from './Base';
 
 /** @class
  *  A troop monster.
  *  @extends System.Base
- *  @param {Record<string, any>} - [json=undefined] Json object describing the 
+ *  @param {Record<string, any>} - [json=undefined] Json object describing the
  *  troop monster
  */
 class TroopMonster extends Base {
+	public id: number;
+	public level: System.DynamicValue;
+	public hidden: System.DynamicValue;
+	public isSpecificPosition: boolean;
+	public specificPosition: System.DynamicValue;
 
-    public id: number;
-    public level: System.DynamicValue;
-    public hidden: System.DynamicValue;
-    public isSpecificPosition: boolean;
-    public specificPosition: System.DynamicValue;
+	constructor(json?: Record<string, any>) {
+		super(json);
+	}
 
-    constructor(json?: Record<string, any>) {
-        super(json);
-    }
-
-    /** 
-     *  Read the JSON associated to the troop monster.
-     *  @param {Record<string, any>} - json Json object describing the troop 
-     *  monster
-     */
-    read(json: Record<string, any>) {
-        this.id = json.id;
-        this.level = System.DynamicValue.readOrDefaultNumber(json.l, 1);
-        this.hidden = System.DynamicValue.readOrDefaultSwitch(json.h, false);
-        this.isSpecificPosition = Utils.defaultValue(json.isSpecificPosition, false);
-        this.specificPosition = System.DynamicValue.readOrDefaultMessage(json
-            .specificPosition, "new Core.Vector3(0,0,0)");
-    }
+	/**
+	 *  Read the JSON associated to the troop monster.
+	 *  @param {Record<string, any>} - json Json object describing the troop
+	 *  monster
+	 */
+	read(json: Record<string, any>) {
+		this.id = json.mid;
+		this.level = System.DynamicValue.readOrDefaultNumber(json.l, 1);
+		this.hidden = System.DynamicValue.readOrDefaultSwitch(json.h, false);
+		this.isSpecificPosition = Utils.defaultValue(json.isSpecificPosition, false);
+		this.specificPosition = System.DynamicValue.readOrDefaultMessage(
+			json.specificPosition,
+			'new Core.Vector3(0,0,0)'
+		);
+	}
 }
 
-export { TroopMonster }
+export { TroopMonster };
