f1713d4d00f8abfe2c7b35fc5c10f83bd34965b9
DESKTOP-2I00R5B\Marie <mmarie.laporte@gmail.com>
2026-03-07 14:21:04 +0100

    Remove useless Libs

--- scripts/update-libs.sh
+++ scripts/update-libs.sh
@@ -4,7 +4,11 @@
 cp ./node_modules/three/build/three.module.min.js ./src/Libs/three.module.min.js
 cp ./node_modules/three/build/three.core.min.js ./src/Libs/three.core.min.js
 rm -rf ./src/Libs/examples
-cp -R ./node_modules/three/examples ./src/Libs
+mkdir -p ./src/Libs/examples/jsm/loaders
+mkdir -p ./src/Libs/examples/jsm/utils
+cp ./node_modules/three/examples/jsm/loaders/GLTFLoader.js ./src/Libs/examples/jsm/loaders/
+cp ./node_modules/three/examples/jsm/utils/BufferGeometryUtils.js ./src/Libs/examples/jsm/utils/
+cp ./node_modules/three/examples/jsm/utils/SkeletonUtils.js ./src/Libs/examples/jsm/utils/
 
 # Howler
 cp ./node_modules/howler/dist/howler.core.min.js ./src/Libs/howler.core.min.js
