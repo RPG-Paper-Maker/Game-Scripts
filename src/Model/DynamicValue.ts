44bbf8d9fbe890911944707333996902a36361db
DESKTOP-2I00R5B\Marie <mmarie.laporte@gmail.com>
2026-05-08 20:29:09 +0200

    Fix crash using variable for map properties music / background song

--- src/Model/DynamicValue.ts
+++ src/Model/DynamicValue.ts
@@ -220,6 +220,7 @@ export class DynamicValue extends Base {
 			case DYNAMIC_VALUE_KIND.VARIABLE:
 				if (!Game.current) {
 					Platform.showErrorMessage('Trying to access a variable value without any game loaded.');
+					return forceVariable ? this.value : 0;
 				}
 				return forceVariable ? this.value : Game.current.getVariable(this.value as number);
 			case DYNAMIC_VALUE_KIND.PARAMETER:
